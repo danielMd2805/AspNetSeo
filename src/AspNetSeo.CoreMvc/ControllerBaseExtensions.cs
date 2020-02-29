1ea4570820f410dc273d7a13c74426adb791a8d6
Daniel Spitzley <daniel@spitzley.eu>
2020-02-29 19:43:05 +0100

    Added HttpContext to SeoController by ctor to pass context from inherit class

--- src/AspNetSeo.CoreMvc/ControllerBaseExtensions.cs
+++ src/AspNetSeo.CoreMvc/ControllerBaseExtensions.cs
@@ -1,21 +1,21 @@
 ﻿using System;
-
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetSeo.CoreMvc
 {
     public static class ControllerBaseExtensions
     {
-        public static ISeoHelper GetSeoHelper(this ControllerBase controller)
+        public static ISeoHelper GetSeoHelper(this ControllerBase controller, HttpContext context)
         {
             if (controller == null)
                 throw new ArgumentNullException(nameof(controller));
 
-            var serviceProvider = controller.HttpContext.RequestServices;
+            var serviceProvider = context.RequestServices;
             if (serviceProvider == null)
             {
                 string message =
-                    $"The {nameof(controller.HttpContext.RequestServices)} of the provided {nameof(ControllerBase)} cannot be null.";
+                    $"The {nameof(context.RequestServices)} of the provided {nameof(ControllerBase)} cannot be null.";
                 throw new ArgumentOutOfRangeException(nameof(controller), message);
             }
 
